5c8aa3732e79c9c059a010d133b40b3af7590ead
qianle <heqianle@hotmail.com>
2017-07-22 23:18:27 -0700

    Binary Tree

---
+++ src/BinaryTree/Identical_Binary_Tree.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+
+public class Identical_Binary_Tree {
+
+    class TreeNode {
+        public int val;
+        public TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+
+    public boolean isIdentical(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+
+        if (a.val != b.val) return false;
+
+        boolean left = isIdentical(a.left, b.left);
+        boolean right = isIdentical(a.right, b.right);
+
+        return left && right;
+    }
+}
